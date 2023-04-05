c84b7961478d3d8276a8228846619439b68d71e9
unknown <kiettt2>
2023-04-05 20:52:53 +0700

    Change job 01

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,10 @@
 pipeline {
     agent any
-    stage('Clone') {
-        steps {
-            git 'https://github.com/kevinram164/Jenkins-Lab.git'
+    stages {
+        stage('Clone') {
+            steps {
+                git 'https://github.com/kevinram164/Jenkins-Lab.git'
+            }
         }
     }
 }
